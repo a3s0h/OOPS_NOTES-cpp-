b3c2428ba0ded5f60aaaf800368554db13cf179e
Nishant Bisht <116999283+a3s0h@users.noreply.github.com>
2024-06-20 19:15:13 +0530

    Update overloading_new_operator.cpp

--- overloading_new_operator.cpp
+++ overloading_new_operator.cpp
@@ -6,16 +6,44 @@ using namespace std;
 // .. here we doing globally 
 void* operator new(size_t size)          // void* is generic because we dont know wht type of memeory user want us to allocate 
 {
+    cout<<"memory is being allocated "<<endl;
     void* ptr = (int*)malloc(size);
     if (!ptr) {
         throw std::bad_alloc();
     }
     return ptr;
-    return ptr;
 }
+
+void operator delete(void* ptr)
+{
+    free(ptr) ;           // this is the way we delete or free up space allocated using malloc/calloc
+}
+
+class temp 
+{
+    private : int data ;
+    public : 
+        temp(): data(0) {
+            cout<<"object created"<<endl;
+        }
+
+        ~temp()
+        {
+            cout<<"destructor called "<<endl;
+        }
+};
 
 
 int main()
 {
+    try {
+        // Allocating object using overloaded new operator
+        temp* obj = new temp;
+        // std::cout << "Object created with a = " << obj->a << ", b = " << obj->b << std::endl;
+        // Deallocating object using overloaded delete operator
+        delete obj;
+    } catch (const std::bad_alloc& e) {
+        std::cerr << "Allocation failed: " << e.what() << std::endl;
+    }
     return 0;
 }
